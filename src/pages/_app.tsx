1a130639ff811e262d8c72dbb8dab064f047c524
Costel Voica <costel.tudor.voica@gmail.com>
2023-04-23 20:32:20 +0300

    created about and books page and added a commented Navbar function in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <ul>
+//         <li><Link href="/">Home</Link></li>
+//         <li><Link href="/books">Books</Link></li>
+//         <li><Link href="/about">About</Link></li>
+//       </ul>
+//     </nav>
+//   )
+// }
+
+// export default Navbar
