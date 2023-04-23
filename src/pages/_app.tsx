b7368a805bac2a11bc2ab5a25ea992bae4dbcca0
Costel Voica <costel.tudor.voica@gmail.com>
2023-04-23 21:10:30 +0300

    Navbar implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Link from 'next/link'
+import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+  <Navbar />
+  <Component {...pageProps} />
+  </>)
+  
 }
-
-
-// function Navbar() {
-//   return (
-//     <nav>
-//       <ul>
-//         <li><Link href="/">Home</Link></li>
-//         <li><Link href="/books">Books</Link></li>
-//         <li><Link href="/about">About</Link></li>
-//       </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
---
+++ src/pages/books.tsx
@@ -0,0 +1,4 @@
+export default function Books() {
+    return <h1>Books</h1>;
+  }
+  
