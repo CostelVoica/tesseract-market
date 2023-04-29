cc6aefba1272fa9e6ab6f9fb977c8018bbe0ac0f
Costel Voica <costel.tudor.voica@gmail.com>
2023-04-29 17:30:53 +0300

    Merge branch 'main' of https://github.com/CostelVoica/scifi-ecommerce

---
+++ src/lib/mongo/db.ts
@@ -0,0 +1,57 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your MongoDB URI to the .env.local file');
+}
+
+let globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<MongoClient>
+}
+
+if (process.env.NODE_ENV === 'development') {
+if (!globalWithMongo._mongoClientPromise) {
+  client = new MongoClient(uri!)
+  globalWithMongo._mongoClientPromise = client.connect()
+}
+  clientPromise = globalWithMongo._mongoClientPromise
+} else {
+  client = new MongoClient(uri!);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
+
+
+// ========================================================
+
+
+// const uri = process.env.MONGODB_URI;
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// };
+
+// let client;
+// let clientPromise;
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Please add your MongoDB URI to the .env.local file');
+// }
+
+// if (process.env.NODE_ENV === 'development') {
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
+
+// export default clientPromise;
