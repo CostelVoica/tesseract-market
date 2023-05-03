c36e5fd3ddc14adce79e85de430b66861b060587
Costel Voica <costel.tudor.voica@gmail.com>
2023-05-04 02:01:10 +0300

    favicon done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href="/favicon/favicon.svg"
+                />
+                <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
