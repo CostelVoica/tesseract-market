f1806d65e32ba5b516846a70da2ad14a9922479d
Costel Voica <costel.tudor.voica@gmail.com>
2023-05-14 00:27:55 +0300

    changes to index/home inside-container

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/layout.module.css'
+import aboutStyles from '../styles/about.module.css'
+import layoutStyles from '../styles/layout.module.css'
+Object.assign(layoutStyles, aboutStyles, layoutStyles)
 
 export default function About() {
     return (
@@ -7,24 +9,24 @@ export default function About() {
             <Head>
                 <title>About</title>
             </Head>
-            <div className={styles['container']}>
-                <div className={styles['center-container']}>
-                    <h1>This is a portfolio project. Its stack is:</h1>
-                    <ul>
-                        <li>
-                            NextJS and Typescript for the frontend/backend logic
-                        </li>
-                        <li>MongoDB as the database for the book info</li>
-                        <li>Tailwind CSS and CSS modules for styling</li>
-                        <li>Stable Diffusion for book cover images</li>
-                        <li>ESLint for the linter</li>
-                        <li>Github for version control</li>
-                    </ul>
+            <div className={layoutStyles['container']}>
+                <div className={layoutStyles['center-container']}>
+                    <div className={aboutStyles['inside-container']}>
+                        <h1>This is a portfolio project. Its stack is:</h1>
+                        <ul>
+                            <li>
+                                NextJS and Typescript for the frontend/backend
+                                logic
+                            </li>
+                            <li>MongoDB as the database for the book info</li>
+                            <li>Tailwind CSS and CSS modules for styling</li>
+                            <li>Stable Diffusion for book cover images</li>
+                            <li>ESLint for the linter</li>
+                            <li>Github for version control</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
-{
-    /* <div className={styles['center-container']}> */
 }
