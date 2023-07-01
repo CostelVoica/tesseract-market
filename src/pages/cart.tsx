3779a95593d0ffaccf73b7f83390ec75dc0345f1
Costel Voica <costel.tudor.voica@gmail.com>
2023-07-02 00:54:09 +0300

    minor changes ==

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import styles from '../styles/layout.module.css'
-let test = {}
+let styles = {}
+import cartStyles from '../styles/about.module.css'
+import layoutStyles from '../styles/layout.module.css'
+Object.assign(layoutStyles, cartStyles, layoutStyles)
 
 export default function Cart() {
     return (
@@ -8,9 +10,11 @@ export default function Cart() {
             <Head>
                 <title>Cart</title>
             </Head>
-            <div className={styles['container']}>
-                <div className={styles['center-container']}>
-                    <h1>The cart is currently a work in progress 🚧 .</h1>
+            <div className={layoutStyles['container']}>
+                <div className={layoutStyles['center-container']}>
+                    <div className={cartStyles['inside-container']}>
+                        <h1>The cart is currently a work in progress 🚧 .</h1>
+                    </div>
                 </div>
             </div>
         </div>
